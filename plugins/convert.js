4631643d203150b0a1f81874e81c9428432ce765
Load-Dilisha <loadilisha@gmail.com>
2025-12-07 19:57:43 +0530

    ⛩️ 킹_딜리샤 ⛩️

--- plugins/convert.js
+++ plugins/convert.js
@@ -166,7 +166,7 @@ try{
       isQuotedImage ? await m.quoted.download(nameJpg) : await m.download(nameJpg)
     let sticker = new Sticker(nameJpg + '.jpg', {
       pack: pushname, // The pack name
-      author: '©VISPER-MD', // The author name
+      author: '킹_딜리샤', // The author name
       type: q.includes("--crop" || '-c') ? StickerTypes.CROPPED : StickerTypes.FULL,
       categories: ["🤩", "🎉"], // The sticker category
       id: "12345", // The sticker id
@@ -230,7 +230,7 @@ try{
       },
     });
 await pipeline(rbg, fs.createWriteStream(namePng + ".png"));
-let dat = `*🌆 VISPER-MD BACKGROUND REMOVER 🌆*
+let dat = `*🌆 킹_딜리샤 BACKGROUND REMOVER 🌆*
 `
 const buttons = [
 {buttonId: prefix + 'rbgi ' + namePng + ".png", buttonText: {displayText: 'IMAGE'}, type: 1},
@@ -279,7 +279,7 @@ try{
 await conn.sendMessage(from, { react: { text: '📥', key: mek.key }})
 let sticker = new Sticker(q, {
   pack: pushname, // The pack name
-  author: 'ɴᴀᴅᴇᴇɴ ᴘᴏᴏʀɴᴀ•', // The author name
+  author: '딜리샤 로드', // The author name
   type: q.includes("--crop" || '-c') ? StickerTypes.CROPPED : StickerTypes.FULL,
   categories: ["🤩", "🎉"], // The sticker category
   id: "12345", // The sticker id
@@ -382,7 +382,7 @@ cmd(
 
       // Convert the sticker buffer to an image (using Sticker class)
       const sticker = new Sticker(stickerBuffer, {
-        pack: "@VISPER-MD",
+        pack: "@딜리샤 로드",
         author: "",
         type: "FULL", // This may not be needed, but ensures we're using the full sticker format
         quality: 100, // Quality of the output image (0-100)
@@ -650,7 +650,7 @@ async (conn, mek, m, { from, args, reply }) => {
         const keywords = packageData.keywords ? packageData.keywords.join(", ") : "No keywords provided";
         // Send the package details as a reply (without image)
         let replyText = `
-*\`💃 VISPER NPM SEARCH 💃\`*
+*\`⛩️ 딜리샤 로드 NPM SEARCH ⛩️\`*
 
 *┌──────────────────*
 *├ 🦑 Npm name :* ${packageName}
